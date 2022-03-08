09531d3fa679a1e1c56a4a2abf3eb06ab22a3a9f
BRABUS <amg430lv@gmail.com>
2022-03-08 02:53:58 +0200

    fix(Todo.vue): add progress bar, dialogs, icons, effects
    BREAKING-CHANGES:
    add progress-bar
    add confirm dialogs
    add append icons delete, complete
    add hover effect on todos
    add toggle (select/open) nodes
    fix computed percentage calculate 0% when todos is empty

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,16 +1,50 @@
 <template>
   <div>
+    <v-progress-linear
+      color="blue-grey"
+      height="25"
+      class="pb-5"
+      :value="percentage"
+    >
+      <template v-slot:default="{ value }">
+        <strong>{{ Math.ceil(value) }}%</strong>
+      </template>
+    </v-progress-linear>
     <InputTodo :nodes_count="this.total_nodes_count" />
-    <p>Selection: {{ selection }}</p>
-    <p>nodes: {{ total_nodes_count }}</p>
     <template v-if="todos.length">
       <v-card tile>
         <v-toolbar color="blue-grey" dark dense flat>
           <v-toolbar-title class="text-body-2"> Todos </v-toolbar-title>
           <v-spacer></v-spacer>
-          <v-btn icon small @click="remove_todos">
-            <v-icon>mdi-delete</v-icon>
+          <v-btn icon small dark @click="toggle_open_nodes">
+            <v-icon>mdi-arrow-expand-vertical</v-icon>
           </v-btn>
+          <v-btn icon small dark @click="toggle_all">
+            <v-icon>mdi-checkbox-multiple-marked-circle</v-icon>
+          </v-btn>
+          <v-dialog v-model="dialog" persistent max-width="290">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn small icon dark v-bind="attrs" v-on="on">
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title class="text-h5"> Delete todos </v-card-title>
+              <v-card-text
+                >Are you sure you want to delete all todos? This cannot be
+                undone.</v-card-text
+              >
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="red darken-1" text @click="dialog = false">
+                  Disagree
+                </v-btn>
+                <v-btn color="green darken-1" text @click="remove_todos">
+                  Agree
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-toolbar>
         <v-card-text>
           <v-treeview
@@ -18,13 +52,39 @@
             :items="todos"
             :selection-type="selection_type"
             :return-object="selection_return_type"
-            open-on-click
+            :open="open_nodes"
             selectable
             transition
+            hoverable
             dark
           >
+            <template v-slot:label="{ item }">
+              <template v-if="item.completed">
+                <del class="green--text text--accent-2">{{ item.name }}</del>
+              </template>
+              <template v-else>
+                <span class="red--text text--accent-2">{{ item.name }}</span>
+              </template>
+            </template>
             <template v-slot:prepend="{ item }">
               <v-icon> {{ item.icon }} </v-icon>
+            </template>
+            <template v-slot:append="{ item }">
+              <v-btn icon small dark class="px-2" @click="update_todo(item.id)">
+                <template v-if="!item.completed">
+                  <v-icon class="red--text text--accent-2"
+                    >mdi-checkbox-blank-circle-outline</v-icon
+                  >
+                </template>
+                <template v-else>
+                  <v-icon class="green--text text--accent-2"
+                    >mdi-checkbox-marked-circle-outline</v-icon
+                  >
+                </template>
+              </v-btn>
+              <v-btn icon small dark class="px-2" @click="remove_todo(item.id)">
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
             </template>
           </v-treeview>
         </v-card-text>
@@ -39,6 +99,7 @@ export default {
   name: "Todo",
   components: { InputTodo },
   data: () => ({
+    dialog: false,
     files: {
       html: "mdi-language-html5",
       js: "mdi-nodejs",
@@ -52,10 +113,33 @@ export default {
   }),
   methods: {
     remove_todos() {
+      this.dialog = false;
       this.$store.dispatch("remove_todos");
+    },
+    remove_todo(id) {
+      console.log("prepared to remove id", id);
+      this.$store.dispatch("remove_todo", id);
+    },
+    update_todo(id) {
+      this.$store.dispatch("update_todo", id);
+      console.log("update todo", id);
+    },
+    toggle_all() {
+      this.$store.dispatch("toggle_all");
+    },
+    toggle_open_nodes() {
+      this.$store.dispatch("toggle_open_nodes");
     },
   },
   computed: {
+    percentage() {
+      if (this.$store.getters.total_todo_nodes === 0) return 0;
+
+      return (
+        (100 * this.$store.getters.completed_nodes) /
+        this.$store.getters.total_todo_nodes
+      );
+    },
     todos: {
       set(todos) {
         this.$store.commit("UPDATE_TODOS", todos);
@@ -89,11 +173,19 @@ export default {
       },
     },
     total_nodes_count: {
+      set(total_nodes) {
+        this.$store.commit("UPDATE_TOTAL_NODES_COUNT", total_nodes);
+      },
       get() {
         return this.$store.getters.total_todo_nodes;
       },
-      set(total_nodes) {
-        this.$store.commit("UPDATE_TOTAL_NODES_COUNT", total_nodes);
+    },
+    open_nodes: {
+      set(nodes) {
+        this.$store.commit("UPDATE_OPEN_NODES", nodes);
+      },
+      get() {
+        return this.$store.getters.open_nodes;
       },
     },
   },
