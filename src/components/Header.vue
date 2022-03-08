affebe99fb467fc5ca96dac6cbd145f2b114e7ca
BRABUS <amg430lv@gmail.com>
2022-03-08 02:14:49 +0200

    fix(Header.vue): add confirmation dialog on reset_todos

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,16 +17,38 @@
 
     <v-spacer></v-spacer>
 
-    <v-btn icon>
-      <v-icon>mdi-plus</v-icon>
-    </v-btn>
+    <v-dialog v-model="dialog" persistent max-width="290">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn dark icon v-bind="attrs" v-on="on">
+          <v-icon>mdi-restart</v-icon>
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title class="text-h5"> Reset todos </v-card-title>
+        <v-card-text
+          >Are you sure you want to delete all todos? This cannot be
+          undone.</v-card-text
+        >
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="red darken-1" text @click="dialog = false">
+            Disagree
+          </v-btn>
+          <v-btn color="green darken-1" text @click="reset_todos">
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app-bar>
 </template>
 
 <script>
 export default {
   name: "Header",
-  data: () => ({}),
+  data: () => ({
+    dialog: false,
+  }),
   computed: {
     drawer: {
       get() {
@@ -38,6 +60,12 @@ export default {
     },
     rnd_bg() {
       return Math.floor(Math.random() * 4) + 1;
+    },
+  },
+  methods: {
+    reset_todos() {
+      this.dialog = false;
+      this.$store.dispatch("reset_todos");
     },
   },
 };
